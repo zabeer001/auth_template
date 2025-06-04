cd69416eae96c26b577854e2b397132a72a302d3
zabeer001 <binzabirtareq@gmail.com>
2025-06-04 14:55:28 +0600

    e

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 
+use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
@@ -12,5 +13,7 @@ Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 Route::get('me', [AuthController::class, 'me'])->middleware('auth:api');
 Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:api');
+
+Route::resource('posts', PostController::class);
 
 
